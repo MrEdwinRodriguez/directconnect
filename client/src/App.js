0cf08e236004bdd251a41ed10922271148289847
Edwin <mredwinrodriguez@gmail.com>
2019-05-07 15:47:58 -0400

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -10,7 +13,14 @@ import './App.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
-
+if (localStorage.jwtToken) {
+  //set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //decode token
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //set user and is authenticated
+  store.dispatch(setCurrentUser(decoded))
+}
 
 function App() {
   return (
