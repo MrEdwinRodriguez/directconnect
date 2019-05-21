00a0c61584053d7a04fde780232d6d7e1a5cf71a
Edwin <mredwinrodriguez@gmail.com>
2019-05-21 09:49:31 -0400

    finished login

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+ 
 import { Provider } from 'react-redux';
+import store from './store';
+ 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-import store from './store'
-
-import './App.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-
+ 
+import './App.css';
+ 
+// Check for token
 if (localStorage.jwtToken) {
-  //set auth token header auth
+  // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
-  //decode token
+  // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
-  //set user and is authenticated
-  store.dispatch(setCurrentUser(decoded))
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+ 
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+ 
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
-
-function App() {
-  return (
-    <Provider store={ store }>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div class='container'>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login }/>
+ 
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
-    </Provider>
-  );
+        </Router>
+      </Provider>
+    );
+  }
 }
-
+ 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -19,6 +19,11 @@ class Register extends Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
  
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
