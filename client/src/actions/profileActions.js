4f134de4123317ad6581878b1ed4e85061e4e962
Edwin <mredwinrodriguez@gmail.com>
2019-06-03 11:38:57 -0400

    edit profile

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
 
 
 //Get current profile
@@ -34,6 +34,25 @@ export const createProfile = (profileData, history) => dispatch => {
             })
             )
 }
+
+//Delete account & profile
+export const deleteAccount = () => dispatch => {
+    if(window.confirm('Are you sure you want to delete your account?  This cannot be undone!')) {
+        axios
+            .delete('/api/profile')
+            .then(res => 
+                dispatch({
+                    type: SET_CURRENT_USER,
+                    payload: {}
+                })
+                ).catch(err => 
+                    dispatch({
+                        type: GET_ERRORS,
+                        payload: err.response.data
+                    })
+                );
+    }
+};
 
 //profile loading
 export const setProfileLoading = () => {
