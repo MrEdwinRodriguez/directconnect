053ea4b30d05ea412c688ab759c5247601aaf04c
Edwin <mredwinrodriguez@gmail.com>
2019-06-04 16:08:03 -0400

    finished profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -15,6 +15,12 @@ class Profile extends Component {
         } 
     }
 
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.profile.profile === null && this.props.profile.loading) {
+            this.props.history.push('/not-found')
+        }
+    }
+
   render() {
     const { profile, loading } = this.props.profile;
     let profileContent;
@@ -31,8 +37,8 @@ class Profile extends Component {
                     <div className='col-md-6'></div>
                 </div>
                 <ProfileHeader profile={profile} />
-                <ProfileAbout />
-                <ProfileCreds />
+                <ProfileAbout profile={profile} />
+                <ProfileCreds education={profile.education} experience={profile.experience}/>
             </div>
         )
     }
