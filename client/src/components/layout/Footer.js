0aac4861612f756dad6e18710ca3f15a362dac92
Edwin <mredwinrodriguez@gmail.com>
2020-06-18 14:39:25 -0400

    fixed footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,8 +3,14 @@ import '../../css/style.css';
 
 export default function Footer() {
   return (
-    <footer className="bg-royal footer text-white mt-5 p-4 text-center">
-    Copyright &copy; { new Date().getFullYear()} Blue and White Connect
-    </footer>
+    // <footer className="bg-royal footer text-white mt-5 p-4 text-center">
+    // Copyright &copy; { new Date().getFullYear()} Blue and White Connect
+    // </footer>
+    <div>
+      <div className='phantom' />
+      <div className="bg-royal footer text-white mt-5 p-4 text-center" >
+          Copyright &copy; { new Date().getFullYear()} Blue and White Connect
+      </div>
+  </div>
   )
 }
