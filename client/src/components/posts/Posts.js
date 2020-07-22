30b3b39103671319bf84e98a582a7970aa273bfe
Edwin <mredwinrodriguez@gmail.com>
2020-07-22 15:27:59 -0400

    welcome message in post

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import PostForm from './PostForm';
@@ -15,7 +16,17 @@ class Posts extends Component {
 
   render() {
     const { posts, loading } = this.props.post;
+    const { profile, auth } = this.props;
     let postContent;
+    let user = auth.user
+    let welcomeMessage = <p>Welcome, {user.first_name} {user.last_name}</p>
+    if(profile && profile.profile && profile.profile.handle) {
+        welcomeMessage = 
+        <p className='lead'>Welcome, <Link to={`/profile/${profile.profile.handle}`}>
+        {user.first_name} {user.last_name}
+        </Link>
+    </p>
+    }
 
     if(posts === null || loading ) {
         postContent = <Spinner />
@@ -26,6 +37,9 @@ class Posts extends Component {
     return (
       <div className='feed'>
         <div className='container'>
+        <div className="row posRelative" >
+            {welcomeMessage}
+        </div>
         <div className='row'>
             <div className='col-md-12'>
             <PinnedItem />
@@ -49,7 +63,9 @@ Posts.propTypes = {
 }
 
 const mapStateToProps = state => ({
-    post: state.post
+    post: state.post,
+    profile: state.profile,
+    auth: state.auth
 })
 
 export default connect(mapStateToProps, { getPosts })(Posts);
