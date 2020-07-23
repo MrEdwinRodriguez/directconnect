1188708713ba0de30046bf24184d517a1a7aa13e
Edwin <mredwinrodriguez@gmail.com>
2020-07-23 14:08:35 -0400

    get podcast and get blog

--- routes/api/content.js
+++ routes/api/content.js
@@ -7,19 +7,42 @@ const Profile = require('../../models/Profile');
 
 //GET API/content/blogs
 //get all blogs
-//private
+//public
 router.get('/blogs', passport.authenticate('jwt', {session: false }), (req, res) => {
-    Profile.find({blog: {$ne: null}}).populate('user').lean().exec()
+    Profile.find({blog: {$ne: null}, hasBlog: true}).populate('user').lean().exec()
     .then(aProfiles => {
         let blogArray = [];
         aProfiles.forEach(profile => {
             let blogObject = {};
-            blogObject.blog = profile.blog;
+            blogObject.name = profile.blog.name;
+            blogObject.link = profile.blog.link;
+            blogObject.about = profile.blog.about;
             blogObject.user = profile.user.name;
             blogObject.email = profile.user.email ? profile.user.email : "";
             blogArray.push(blogObject)
         })
         res.json({blogs: blogArray})
+    })
+    .catch(err => res.status(404).json(err));
+})
+
+//GET API/content/podcast
+//get all podcast
+//public
+router.get('/podcast', passport.authenticate('jwt', {session: false }), (req, res) => {
+    Profile.find({podcast: {$ne: null}, hasPodcast: true}).populate('user').lean().exec()
+    .then(aProfiles => {
+        let podcastArray = [];
+        aProfiles.forEach(profile => {
+            let podcastObject = {};
+            podcastObject.name = profile.podcast.name;
+            podcastObject.link = profile.podcast.link;
+            podcastObject.about = profile.podcast.about;
+            podcastObject.user = profile.user.name;
+            podcastObject.email = profile.user.email ? profile.user.email : "";
+            podcastArray.push(podcastObject)
+        })
+        res.json({podcasts: podcastArray})
     })
     .catch(err => res.status(404).json(err));
 })
